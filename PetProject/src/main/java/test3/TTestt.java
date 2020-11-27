5b9cdb889665b8796314eeebac57418f57ec4dc9
CanCan-Wu <s3439000@gmail.com>
2020-11-27 14:47:07 +0800

    HI

--- PetProject/src/main/java/test3/TTestt.java
+++ PetProject/src/main/java/test3/TTestt.java
@@ -4,7 +4,8 @@ public class TTestt {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("777");
+			System.out.println("777");
+			System.out.println("HI");
 	}
 
 }

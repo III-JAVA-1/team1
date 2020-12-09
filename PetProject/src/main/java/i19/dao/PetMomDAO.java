a47d72532f1ad230b8499462f7139a9d7181bfc3
zseqw123 <sakuya111235@gmail.com>
2020-12-09 22:04:35 +0800

    i19

---
+++ PetProject/src/main/java/i19/dao/PetMomDAO.java
@@ -0,0 +1,21 @@
+package i19.dao;
+
+import i19.model.MomBean;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import util.HibernateUtils;
+
+/**
+ * @author i19
+ */
+public class PetMomDAO {
+    SessionFactory factory = HibernateUtils.getSessionFactory();
+
+    public int save(MomBean mb) {
+        int count=0;
+        Session session = factory.getCurrentSession();
+        session.save(mb);
+        count++;
+        return count;
+    }
+}

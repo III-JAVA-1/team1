5a3138b14cb49e2ed2706d508fe082a9bb5ecb7b
zseqw123 <sakuya111235@gmail.com>
2021-01-04 19:31:21 +0800

    訂單評論 評分 評論評分初建

---
+++ src/main/java/com/web/pet/mom/dao/OrderCommentDAO.java
@@ -0,0 +1,57 @@
+package com.web.pet.mom.dao;
+
+
+import com.web.pet.mom.model.OrderComment;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author i19
+ */
+@Repository
+public class OrderCommentDAO {
+    SessionFactory sessionFactory;
+
+    @Autowired
+    public OrderCommentDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public void insertOrderComment(OrderComment raing) { //新增評論
+        Session session = sessionFactory.getCurrentSession();
+        session.save(raing);
+    }
+
+
+    public List<OrderComment> searchOrderComment(int uId) { //查詢一筆評論
+        List<OrderComment> list = new ArrayList<>();
+        String hql = "FROM OrderComment WHERE mom_Id = :uId";
+        Query query = sessionFactory.getCurrentSession().createQuery(hql).setParameter("momId", uId);
+        list = query.getResultList();
+        return list;
+    }
+
+    public int updateOrderComment(OrderComment orderComment) {//修改評論
+        int count = 0;
+        Session session = sessionFactory.getCurrentSession();
+        session.update(orderComment);
+        count++;
+        return count;
+    }
+
+    public int deleteOrderComment(OrderComment orderComment) {//刪除評論
+        int count = 0;
+        Session session = sessionFactory.getCurrentSession();
+        session.delete(orderComment);
+        count++;
+        return count;
+    }
+
+
+}

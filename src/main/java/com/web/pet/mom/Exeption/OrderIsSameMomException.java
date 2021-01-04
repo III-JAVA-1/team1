16632dfa99f351c8175c8f810a065b6ba208b480
zseqw123 <sakuya111235@gmail.com>
2021-01-04 19:33:51 +0800

    預約保母 例外 1.初建

---
+++ src/main/java/com/web/pet/mom/Exeption/OrderIsSameMomException.java
@@ -0,0 +1,16 @@
+package com.web.pet.mom.Exeption;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author i19
+ */
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class OrderIsSameMomException extends RuntimeException{
+    OrderIsSameMomException(){
+        super("自己無法預約自己");
+    }
+}
+
+
